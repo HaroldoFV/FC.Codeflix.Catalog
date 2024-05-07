691cf7677554d822e70d167d67ff3037b024dbcd
Haroldo Vinente <haroldovinente@gmail.com>
2024-05-07 16:01:54 -0300

    finishing DomainValidationTest

--- tests/FC.Codeflix.Catalog.UnitTests/Validation/DomainValidationTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Validation/DomainValidationTest.cs
@@ -147,4 +147,30 @@ public class DomainValidationTest
             yield return new object[] {example, maxLength};
         }
     }
+
+    [Theory(DisplayName = nameof(maxLengthOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [MemberData(nameof(GetValuesLessThanMax), parameters: 10)]
+    public void maxLengthOk(string target, int maxLength)
+    {
+        string fieldName = Faker.Commerce.ProductName().Replace(" ", "");
+
+        Action action =
+            () => DomainValidation.MaxLength(target, maxLength, fieldName);
+
+        action.Should().NotThrow();
+    }
+
+    public static IEnumerable<object[]> GetValuesLessThanMax(int numberOftests = 5)
+    {
+        yield return new object[] {"123456", 6};
+        var faker = new Faker();
+
+        for (int i = 0; i < (numberOftests - 1); i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var maxLength = example.Length + (new Random()).Next(0, 5);
+            yield return new object[] {example, maxLength};
+        }
+    }
 }
