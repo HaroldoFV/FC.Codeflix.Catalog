a20a39ffab8583c003b7f626db302fab24a51f9f
Haroldo Vinente <haroldovinente@gmail.com>
2024-05-09 11:07:42 -0300

    Conclusão - CreateCategoryTest

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Category/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Category/CreateCategory/CreateCategoryTest.cs
@@ -1,3 +1,4 @@
+using FC.Codeflix.Catalog.Domain.Exceptions;
 using FluentAssertions;
 using Moq;
 using UseCases = FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
@@ -42,10 +43,105 @@ public class CreateCategoryTest(CreateCategoryTestFixture fixture)
         output.CreatedAt.Should().NotBeSameDateAs(default);
     }
 
+    [Fact(DisplayName = nameof(CreateCategoryWithOnlyName))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategoryWithOnlyName()
+    {
+        var repositoryMock = fixture.GetRepositoryMock();
+        var unitOfWorkMock = fixture.GetUnitOfWorkMock();
+
+        var useCase = new UseCases.CreateCategory(
+            repositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+
+        var input = new UseCases.CreateCategoryInput(
+            fixture.GetValidCategoryName()
+        );
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(
+            repository => repository.Insert(
+                It.IsAny<DomainEntity.Category>(),
+                It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+
+        unitOfWorkMock.Verify(
+            uow => uow.Commit(It.IsAny<CancellationToken>()),
+            Times.Once
+        );
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be("");
+        output.IsActive.Should().BeTrue();
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+    }
+
+    [Fact(DisplayName = nameof(CreateCategoryWithOnlyNameAndDescription))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategoryWithOnlyNameAndDescription()
+    {
+        var repositoryMock = fixture.GetRepositoryMock();
+        var unitOfWorkMock = fixture.GetUnitOfWorkMock();
+
+        var useCase = new UseCases.CreateCategory(
+            repositoryMock.Object,
+            unitOfWorkMock.Object
+        );
+
+        var input = new UseCases.CreateCategoryInput(
+            fixture.GetValidCategoryName(),
+            fixture.GetValidCategoryDescription()
+        );
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(
+            repository => repository.Insert(
+                It.IsAny<DomainEntity.Category>(),
+                It.IsAny<CancellationToken>()
+            ),
+            Times.Once
+        );
+
+        unitOfWorkMock.Verify(
+            uow => uow.Commit(It.IsAny<CancellationToken>()),
+            Times.Once
+        );
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().BeTrue();
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+    }
 
     [Theory(DisplayName = nameof(ThrowWhenCantInstantiateAggregate))]
     [Trait("Application", "CreateCategory - Use Cases")]
-    public void ThrowWhenCantInstantiateAggregate()
+    [MemberData(
+        nameof(CreateCategoryTestDataGenerator.GetInvalidInputs),
+        parameters: 24,
+        MemberType = typeof(CreateCategoryTestDataGenerator)
+    )]
+    public async void ThrowWhenCantInstantiateAggregate(
+        UseCases.CreateCategoryInput input,
+        string exceptionMessage
+    )
     {
+        var useCase = new UseCases.CreateCategory(
+            fixture.GetRepositoryMock().Object,
+            fixture.GetUnitOfWorkMock().Object
+        );
+
+        Func<Task> task = async () =>
+            await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should()
+            .ThrowAsync<EntityValidationException>()
+            .WithMessage(exceptionMessage);
     }
 }
