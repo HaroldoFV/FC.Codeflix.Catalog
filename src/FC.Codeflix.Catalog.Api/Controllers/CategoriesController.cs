944475e163638eb7b1459e66b872dbd0d2961194
Haroldo Vinente <haroldovinente@gmail.com>
2024-07-24 10:57:07 -0300

    feat(categorias): adiciona controller de Categorias com endpoint de criação
    - Cria CategoriesController
    - Implementa endpoint para criar novas categorias

---
+++ src/FC.Codeflix.Catalog.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+using FC.Codeflix.Catalog.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FC.Codeflix.Catalog.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoriesController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(CategoryModelOutput), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+    public async Task<IActionResult> Create([FromBody] CreateCategoryInput input, CancellationToken cancellationToken)
+    {
+        var output = await mediator.Send(input, cancellationToken);
+        return CreatedAtAction(nameof(Create), new { output.Id }, output);
+    }
+}
